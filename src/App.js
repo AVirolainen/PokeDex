8e356953870b9a568925690a0389fe1fc57ade23
Stanislav <rudenkostas2@gmail.com>
2021-06-02 14:51:06 +0300

    init\

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ViewBox from "./components/ViewBox"
+import logo from "./assets/pokedex_logo.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">      
+        <img className="logo" src={logo} alt="logo"/>
+        <ViewBox />
+      </div>
+    
   );
 }
 
---
+++ src/components/ViewBox.js
@@ -0,0 +1,12 @@
+import './ViewBox.css';
+
+const ViewBox = ()=>{
+  return (
+    <div className="viewBox">
+        
+
+    </div>
+  );
+}
+
+export default ViewBox;
