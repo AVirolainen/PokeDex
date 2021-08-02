114911c75b0a3682888e9b1d1f6eb3541f47a8e3
Rudenko Stanislav <rudenkostas2@gmail.com>
2021-08-02 14:13:07 +0300

    adding carousel

--- src/components/ViewBox.js
+++ src/components/ViewBox.js
@@ -2,7 +2,6 @@ import './ViewBox.css';
 import sandshrew from "../assets/po/sandshrew.jpg"
 import bulbasaur from "../assets/po/bulbasaur.jpg"
 
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import {
     BrowserRouter as Router,
@@ -11,8 +10,12 @@ import {
     Link
   } from "react-router-dom";
 
+
 import RandomPage from "./RandomPage"
 import InfoPage from "./InfoPage"
+
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ViewBox = (props)=>{
 
@@ -23,18 +26,23 @@ const ViewBox = (props)=>{
                 <Route exact path="/">
                 
                 <div className="boxWrapper">
-                    <div className="boxImage">
-                        <img src={sandshrew} alt="logo"/>
-                        
-                            <Link to="/random" style={{ textDecoration: 'none' }}>
-                            <div className="route-button random">
-                                    Show random Pokemon   
-                              </div>
-                            </Link>
-              
-                    </div>
-                    <div className="boxImage">
-                        <img src={bulbasaur} alt="logo"/>
+
+                <Carousel showThumbs={false} dynamicHeight={false} width={600} showStatus={false} autoPlay={true} infiniteLoop={true} showArrows={false}>
+
+                <div className="boxImage">
+                    <img src={sandshrew} alt="logo"/>
+                    
+                        <Link to="/random" style={{ textDecoration: 'none' }}>
+                        <div className="route-button random">
+                                Show random Pokemon   
+                            </div>
+                        </Link>
+            
+                </div>
+
+
+                <div className="boxImage">
+                    <img src={bulbasaur} alt="logo"/>
                     <div className="wrapper">
                         <div className="outlined-basic">
                             <TextField id="outlined-basic" variant="outlined" />
@@ -51,7 +59,9 @@ const ViewBox = (props)=>{
                             </Link>
                           
                     </div>
-                    </div>
+                </div>
+
+                </Carousel>
 
 
                 </div>
